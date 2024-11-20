8e1ade5562b7f45024827f4cedf1f2ff90625fc5
ILoveRice07 <159123610+ILoveRice07@users.noreply.github.com>
2024-11-20 11:30:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ array = [4, 98, 7, 24, 16, 0, 37, 63, 52, 86]
 #Použití jednoduchého sortu k třídění pole
 array.sort()
 print(array)
+
+#Tvorba bubble sortu
+def bubble_sort():
+    n = len(array)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+            print(array)
+    return array
+
+print(bubble_sort())
